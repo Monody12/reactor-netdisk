4ea830eb7429d46a4c1506a69531bf2952f0a725
monody <614908309@qq.com>
2024-07-12 16:07:05 +0800

    为DTO添加注释

--- src/main/kotlin/com/example/reactornetdisk/dto/FolderDTO.kt
+++ src/main/kotlin/com/example/reactornetdisk/dto/FolderDTO.kt
@@ -1,5 +1,8 @@
 package com.example.reactornetdisk.dto
 
+/**
+ * DTO for creating a folder
+ */
 data class FolderDTO(
     val name: String,
     val parentId: Long? = null
--- src/main/kotlin/com/example/reactornetdisk/dto/UserDTO.kt
+++ src/main/kotlin/com/example/reactornetdisk/dto/UserDTO.kt
@@ -1,5 +1,8 @@
 package com.example.reactornetdisk.dto
 
+/**
+ * DTO for user login
+ */
 data class UserDTO(
     val username: String,
     val password: String,
